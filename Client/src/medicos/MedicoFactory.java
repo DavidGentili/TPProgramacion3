07bb4a238185e2c8bd67e71c58fee6e272b481de
DavidGentili <Davidgenntili@gmail.com>
2021-05-05 15:05:38 -0300

    reinicio

--- Client/src/medicos/MedicoFactory.java
+++
@@ -1,8 +0,0 @@
-package medicos;
-
-public class MedicoFactory {
-
-	public MedicoFactory() {
-
-	}
-}
--- Client/src/pacientes/IPaciente.java
+++
@@ -1,9 +0,0 @@
-package pacientes;
-
-public interface IPaciente {
-	
-	public abstract IPaciente ingreso(IPaciente otro);
-	public abstract IPaciente comparaConNiño(IPaciente otro);
-	public abstract IPaciente comparaConJoven(IPaciente otro);
-	public abstract IPaciente comparaConMayor(IPaciente otro);
-}
