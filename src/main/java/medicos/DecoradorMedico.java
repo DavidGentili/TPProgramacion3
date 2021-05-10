c1e722bbac2f68455e1ac4235b190f66523b2983
DavidGentili <Davidgenntili@gmail.com>
2021-05-10 11:42:26 -0300

    JavaDoc pack medicos generado

--- src/main/java/medicos/DecoradorMedico.java
+++ src/main/java/medicos/DecoradorMedico.java
@@ -2,6 +2,12 @@ package medicos;
 
 import personas.Domicilio;
 
+/**
+ * 
+ * Es la clase padre de todos los elementos del patron decorator, implementado a
+ * los medicos
+ *
+ */
 public abstract class DecoradorMedico implements IMedico {
 
 	protected IMedico encapsulado;
@@ -10,39 +16,63 @@ public abstract class DecoradorMedico implements IMedico {
 		this.encapsulado = encapsulado;
 	}
 
+	/**
+	 * Retorna el sueldo de los medicos
+	 */
 	@Override
 	public abstract double getSueldo();
 
+	/**
+	 * Retorna el nombre del medico encapsulado
+	 */
 	@Override
 	public String getNombre() {
 		return encapsulado.getNombre();
 	}
 
+	/**
+	 * Retorna el apellido del medico encapsulado
+	 */
 	@Override
 	public String getApellido() {
 		return encapsulado.getApellido();
 	}
 
+	/**
+	 * Retorna el DNI del medico encapsulado
+	 */
 	@Override
 	public int getDni() {
 		return encapsulado.getDni();
 	}
 
+	/**
+	 * Retorna el telefono del medico encapsulado
+	 */
 	@Override
 	public String getTelefono() {
 		return encapsulado.getTelefono();
 	}
 
+	/**
+	 * Retorna el domicilio del medico encapsulado
+	 */
 	@Override
 	public Domicilio getDomicilio() {
 		return encapsulado.getDomicilio();
 	}
 
+	/**
+	 * Retorna la ciudad del medico encapsulado
+	 */
 	@Override
 	public String getCiudad() {
 		return encapsulado.getCiudad();
 	}
 
+	/**
+	 * Retorna un string con la informacion del medico encapsulado
+	 */
 	@Override
 	public String toString() {
 		return encapsulado.toString();
@@ -54,5 +84,4 @@ public abstract class DecoradorMedico implements IMedico {
 		return encapsulado.getMatricula();
 	}
 
-	
 }
--- src/main/java/medicos/IMedico.java
+++ src/main/java/medicos/IMedico.java
@@ -2,23 +2,75 @@ package medicos;
 
 import personas.Domicilio;
 
+/**
+ * 
+ * Insteface con los metodos necesarios para que los elementos del patron
+ * Decorator interactuen con su encapsulado
+ *
+ */
 public interface IMedico {
+
+	/**
+	 * Retorna el Sueldo bruto del medico
+	 * 
+	 * @return
+	 */
 	double getSueldo();
 
+	/**
+	 * Retorna el nombre del medico
+	 * 
+	 * @return
+	 */
 	String getNombre();
 
+	/**
+	 * Retorna el apellido del medico
+	 * 
+	 * @return
+	 */
 	String getApellido();
 
+	/**
+	 * Retorna el DNI del medico
+	 * 
+	 * @return
+	 */
 	int getDni();
-	
+
+	/**
+	 * Retorna la matricula del medico
+	 * 
+	 * @return
+	 */
 	int getMatricula();
 
+	/**
+	 * Retorna el telefono del medico
+	 * 
+	 * @return
+	 */
 	String getTelefono();
 
+	/**
+	 * Retorna el domicilio del medico
+	 * 
+	 * @return
+	 */
 	Domicilio getDomicilio();
 
+	/**
+	 * Retorna la ciudad del medico
+	 * 
+	 * @return
+	 */
 	String getCiudad();
 
+	/**
+	 * Retorna un string con la informacion del medico
+	 * 
+	 * @return
+	 */
 	String toString();
 
 }
