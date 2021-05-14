eef20a550b47c81d1bbe228ef77968163b7ee6d3
DavidGentili <Davidgenntili@gmail.com>
2021-05-14 17:49:34 -0300

    Excepcion paciente Factory

---
+++ src/main/java/exceptions/TipoDePacienteIncorrectoException.java
@@ -0,0 +1,16 @@
+package exceptions;
+
+public class TipoDePacienteIncorrectoException extends Exception {
+
+	public TipoDePacienteIncorrectoException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public TipoDePacienteIncorrectoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
--- src/main/java/pacientes/PacienteFactory.java
+++ src/main/java/pacientes/PacienteFactory.java
@@ -1,29 +1,36 @@
 package pacientes;
 
+import exceptions.TipoDePacienteIncorrectoException;
 import personas.Domicilio;
 
 public class PacienteFactory {
-	public PacienteFactory() {}
-	
-	public static Paciente getInstance(String tipo, String nombre, String apellido, int dni, String telefono, Domicilio domicilio, String ciudad, int historiaClinica) {
-		Paciente respuesta=null;
-		if(tipo.equalsIgnoreCase("ninio"))
-			respuesta=new Ninio(nombre,apellido,dni,telefono,domicilio,ciudad,historiaClinica);
-		else if(tipo.equalsIgnoreCase("joven"))
-			respuesta=new Joven(nombre,apellido,dni,telefono,domicilio,ciudad,historiaClinica);
-		else if(tipo.equalsIgnoreCase("mayor"))
-			respuesta=new Mayor(nombre,apellido,dni,telefono,domicilio,ciudad,historiaClinica);
-		return respuesta;
+	public PacienteFactory() {
 	}
-	
-	public static Paciente getInstance(String tipo, String nombre, String apellido, int dni, int historiaClinica){
-		Paciente respuesta=null;
-		if(tipo.equalsIgnoreCase("ninio"))
-			respuesta=new Ninio(nombre,apellido,dni,historiaClinica);
-		else if(tipo.equalsIgnoreCase("joven"))
-			respuesta=new Joven(nombre,apellido,dni,historiaClinica);
-		else if(tipo.equalsIgnoreCase("mayor"))
-			respuesta=new Mayor(nombre,apellido,dni,historiaClinica);
+
+	public static Paciente getInstance(String tipo, String nombre, String apellido, int dni, String telefono,
+			Domicilio domicilio, String ciudad, int historiaClinica) throws TipoDePacienteIncorrectoException{
+		Paciente respuesta = null;
+		if (tipo.equalsIgnoreCase("ninio") || tipo.equalsIgnoreCase("niño"))
+			respuesta = new Ninio(nombre, apellido, dni, telefono, domicilio, ciudad, historiaClinica);
+		else if (tipo.equalsIgnoreCase("joven"))
+			respuesta = new Joven(nombre, apellido, dni, telefono, domicilio, ciudad, historiaClinica);
+		else if (tipo.equalsIgnoreCase("mayor"))
+			respuesta = new Mayor(nombre, apellido, dni, telefono, domicilio, ciudad, historiaClinica);
+		else
+			throw new TipoDePacienteIncorrectoException("Rango etareo ingresado incorrecto");
+			return respuesta;
+	}
+
+	public static Paciente getInstance(String tipo, String nombre, String apellido, int dni, int historiaClinica) throws TipoDePacienteIncorrectoException {
+		Paciente respuesta = null;
+		if (tipo.equalsIgnoreCase("ninio") || tipo.equalsIgnoreCase("niño"))
+			respuesta = new Ninio(nombre, apellido, dni, historiaClinica);
+		else if (tipo.equalsIgnoreCase("joven"))
+			respuesta = new Joven(nombre, apellido, dni, historiaClinica);
+		else if (tipo.equalsIgnoreCase("mayor"))
+			respuesta = new Mayor(nombre, apellido, dni, historiaClinica);
+		else
+			throw new TipoDePacienteIncorrectoException("Rango etareo ingresado incorrecto");
 		return respuesta;
 	}
 
